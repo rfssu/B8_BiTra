a2170aa00a1b21bde9f9df8690de20805c00aaee
Activity_4 <egyputra78@gmail.com>
2023-06-28 01:36:40 +0700

    membuat controller untuk menambah pesanan

---
+++ lib/controller/pesanan_controller.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:project_bitra/model/model_pesanan.dart';
+
+class PesananController {
+  final pesananCollection = FirebaseFirestore.instance.collection('pesanan');
+
+  final StreamController<List<DocumentSnapshot>> streamController =
+      StreamController<List<DocumentSnapshot>>.broadcast();
+
+  Stream<List<DocumentSnapshot>> get stream => streamController.stream;
+
+  Future addpesanan(PesananModel modelPesanan) async {
+    final pesanan = modelPesanan.toMap();
+
+    final DocumentReference docRef = await pesananCollection.add(pesanan);
+
+    final String docId = docRef.id;
+
+    final PesananModel pesananModel = PesananModel(
+        id: docId,
+        nama:modelPesanan.nama,
+        alamat:modelPesanan.alamat,
+        noHp:modelPesanan.noHp,
+        namaBarang:modelPesanan.namaBarang,
+        jumlahBarang:modelPesanan.jumlahBarang,
+        tanggal:modelPesanan.tanggal);
+
+    await docRef.update(pesananModel.toMap());
+  }
+   Future getPesanan() async {
+    final pesanan = await pesananCollection.get();
+    streamController.sink.add(pesanan.docs);
+
+    return pesanan.docs;
+  }
+
+  Future deleteContact(String id) async {
+    final contact = await pesananCollection.doc(id).delete();
+    return contact;
+  }
+}
