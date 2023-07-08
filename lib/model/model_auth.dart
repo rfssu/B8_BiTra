c0af01c54fce9fbaf1150291353f05a571832aad
Activity_4 <egyputra78@gmail.com>
2023-07-08 21:19:25 +0700

    membuat model untuk auth

--- lib/model/model_auth.dart
+++ lib/model/model_auth.dart
@@ -1,31 +1,34 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-class userModel {
-  String UserName;
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class UserModel {
+  String userName;
   String email;
   String uId;
   String role;
-  userModel({
-    required this.UserName,
+  UserModel({
+    required this.userName,
     required this.email,
     required this.uId,
     required this.role,
   });
-
+ 
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'UserName': UserName,
+      'userName': userName,
       'email': email,
       'uId': uId,
       'role': role,
     };
   }
 
-  factory userModel.fromMap(Map<String, dynamic> map) {
-    return userModel(
-      UserName: map['UserName'] as String,
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      userName: map['userName'] as String,
       email: map['email'] as String,
       uId: map['uId'] as String,
       role: map['role'] as String,
@@ -34,5 +37,6 @@ class userModel {
 
   String toJson() => json.encode(toMap());
 
-  factory userModel.fromJson(String source) => userModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  static UserModel? fromFirebaseUser(User user) {}
 }
