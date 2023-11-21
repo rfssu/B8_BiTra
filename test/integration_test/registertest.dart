24881221194ca93ef7238e29bb94bfb8289d4583
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2023-11-21 20:43:13 +0700

    Commit registertest

---
+++ test/integration_test/registertest.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:project_bitra/View/registerpage.dart';
+import 'package:test/integration_test/registertest.dart';
+import 'package:project_bitra/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('Register Test', () {
+    testWidgets(
+      'verify validasi input data register',
+      (tester) async {
+        app.main();
+        
+        await tester.pumpAndSettle();
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.tap(find.byType(TextButton));
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.enterText(find.byType(TextFormField).at(0), 'Airel Javier Aldaffae');
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.enterText(find.byType(TextFormField).at(1), 'sleman');
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.enterText(find.byType(TextFormField).at(2), 'email@gmail.com');
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.enterText(find.byType(TextFormField).at(3), '12345678');
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.tap(find.byType(ElevatedButton));
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.pumpAndSettle();
+
+        await Future.delayed(const Duration(seconds: 2));
+        expect(find.byType(RegisterPage), findsOneWidget);
+      },
+    );
+   });
+}
