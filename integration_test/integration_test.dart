5f62e2b79e95787daf420b51aed1701a46f82244
MuhammadIrsyad06 <muhammadirsyad764@gmail.com>
2023-11-22 22:24:28 +0700

    update integtest

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -1,34 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:project_bitra/View/loginScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project_bitra/View/registerpage.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:project_bitra/View/registerpage.dart';
 import 'package:project_bitra/main.dart' as app;
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  group('Register Test', () {
-    testWidgets(
-      'verify validasi input data register',
-      (tester) async {
-        app.main();
-        
-        await tester.pumpAndSettle();
-        await Future.delayed(const Duration(seconds: 2));
-        await tester.tap(find.byType(TextButton));
-        await Future.delayed(const Duration(seconds: 2));
-        await tester.enterText(find.byType(TextFormField).at(0), 'Airel Javier Aldaffae');
-        await Future.delayed(const Duration(seconds: 2));
-        await tester.enterText(find.byType(TextFormField).at(2), 'email@gmail.com');
-        await Future.delayed(const Duration(seconds: 2));
-        await tester.enterText(find.byType(TextFormField).at(3), '12345678');
-        await Future.delayed(const Duration(seconds: 2));
-        await tester.tap(find.byType(ElevatedButton));
-        await Future.delayed(const Duration(seconds: 2));
-        await tester.pumpAndSettle();
+  testWidgets('Register widget UI test', (WidgetTester tester) async{
+    await Firebase.initializeApp();
+    await tester.pumpWidget(const MaterialApp(
+      home: RegisterPage(),));
 
-        await Future.delayed(const Duration(seconds: 2));
-        expect(find.byType(RegisterPage), findsOneWidget);
-      },
-    );
-   });
+      expect(find.text('Name'),findsOneWidget);
+      expect(find.text('Email'),findsOneWidget);
+      expect(find.text('Password'),findsOneWidget);
+      expect(find.text('Register'),findsOneWidget);
+      expect(find.text('textLogin'),findsOneWidget);
+
+      await tester.enterText(find.byType(TextFormField).first,'icad');
+      await Future.delayed(const Duration(seconds: 2));
+
+      await tester.enterText(find.byType(TextFormField).at(1),'icad@gmail.com');
+      await Future.delayed(const Duration(seconds: 2));
+
+      await tester.enterText(find.byType(TextFormField).last,'12345678');
+      await Future.delayed(const Duration(seconds: 2));
+
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle();
+      await Future.delayed(const Duration(seconds: 2));
+
+      await tester.tap(find.text('Register'));
+      await tester.pumpAndSettle();
+      await Future.delayed(const Duration(seconds: 2));
+
+      expect(find.text('Registration Successful'),findsOneWidget);
+      expect(find.text('OK'), findsOneWidget);
+
+      await tester.tap(find.text('OK'));
+      await tester.pumpAndSettle();
+      await Future.delayed(const Duration(seconds: 2));
+
+      expect(find.byType(LoginPage), findsOneWidget);
+      await tester.pumpAndSettle(const Duration(seconds: 5));
+  });
 }
